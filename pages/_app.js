be099d824b807439499a8b2113307156fb7acc1f
Hembunos <hembunospro1428@gmail.com>
2024-08-01 11:55:10 +0530

    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 
 import { SocketProvider } from "@/context/socket";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
   return (
     <SocketProvider>
       <Component {...pageProps} />
+      <Analytics />
     </SocketProvider>
   );
 }
